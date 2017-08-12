1f3238c17d0dc85a3ca5aa570705e874901b093a
Melissa Lopez <lopezme17@gmail.com>
2017-08-11 22:09:22 -0700

    Created routing for agendas

--- Rengine/server.js
+++ Rengine/server.js
@@ -57,3 +57,55 @@ activities.route('/:actId')
 
 app.use('/act',activities);
 
+var agenda = express.Router();
+
+agenda.use(bodyParser.json());
+
+agenda.route('/')
+    .all(function(req,res,next) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        next();
+    })
+
+    .get(function(req,res,next){
+        res.end('All agendas are being sent!');
+    })
+
+// .post(function(req, res, next){
+//     res.end('Adding the following activity' + req.body.name + ' with details: ' + req.body.description);
+// })
+//
+// .delete(function(req, res, next){
+//     res.end('Deleting all actvities');
+// });
+
+agenda.route('/:agendaId')
+    .all(function(req,res,next) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        next();
+    })
+
+    .get(function(req,res,next){
+        res.end('Will get the following activity details: ' + req.params.agendaId +' to you!');
+    })
+
+    .put(function(req, res, next){
+        res.write('Updating the followingactiviy ' + req.params.agendaId + '\n');
+        res.end('Will update the activity: ' + req.body.name +
+            ' with details: ' + req.body.description);
+    })
+
+    .delete(function(req, res, next){
+        res.end('Deleting activiity: ' + req.params.agendaId);
+    });
+
+app.use('/agendas', agenda);
+
+
+
+// app.use(express.static(__dirname + '/public'));
+//
+// app.listen(port, hostname, function(){
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
